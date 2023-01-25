f3f7d58135a93442c55d55c7c235d35a767ef920
Mugamba669 <brunohectre@gmail.com>
2023-01-25 18:30:56 +0300

    Added required filles

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+## This is a simple java servlet application of a Vaccination Tracking system
+
+### To acces it
+
+```
+ git clone https://github.com/Mugamba669/Vaccination-tracker.git
+```
+
+## Stack used
+- Java
---
+++ src/java/org/health/administration/Patients.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/TagHandler.java to edit this template
+ */
+package org.health.administration;
+
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+/**
+ *
+ * @author mugamba
+ */
+public class Patients extends SimpleTagSupport {
+
+    private String table;
+    private String values;
+
+    /**
+     * Called by the container to invoke this tag. The implementation of this
+     * method is provided by the tag library developer, and handles all tag
+     * processing, body iteration, etc.
+     */
+    @Override
+    public void doTag() throws JspException {
+        JspWriter out = getJspContext().getOut();
+        
+        try {
+            
+
+            JspFragment f = getJspBody();
+            if (f != null) {
+                f.invoke(out);
+            }
+
+        } catch (java.io.IOException ex) {
+            throw new JspException("Error in Patients tag", ex);
+        }
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public void setValues(String values) {
+        this.values = values;
+    }
+    
+}
